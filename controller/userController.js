9e493133c267c1d0c9650f13b775ecdcfee7c327
vinay-stacks <vinaykp1991@gmail.com>
2025-05-23 23:12:10 +0530

    first commit

--- controller/userController.js
+++ controller/userController.js
@@ -60,3 +60,43 @@ export const getUserById = async (req, res)=>{
 }
 
 // update user data
+
+export const updateUserData = async (req, res)=>{
+  try{
+     const id =req.params.id;
+     const userUpdateData = await User.findById(id);
+     console.log("user data", userUpdateData);
+     if(!userUpdateData){
+      return res.status(400).json({message: 'user not found'})
+     }
+     await User.findByIdAndUpdate(id, req.body, {
+      new:true
+     })
+
+     res.status(200).json({message: "user data update successfully", data:userUpdateData})
+
+  }catch(err){
+    console.log("error updating user data:", err);
+    res.status (500).json({message: "internal server error"});
+  }
+}
+
+// delete user data
+
+export const deleteUserData = async(req, res)=>{
+  try{
+    const id = req.params.id;
+    const userExistData = await User.findById(id);
+    console.log("user data", userExistData);
+    if(!userExistData){
+      return res.status(400).json({message: "User Data Not Found"})
+    }
+    await  User.findByIdAndDelete(id);
+
+    res.status (200).json({message: "User Data deleted successfully", data: userExistData})
+     
+  }catch(err){
+    console.log("error deleting user data:", err);
+    res.status(500).json({message:"internal server error"});
+  }
+}
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,11 +1,13 @@
 import express from 'express';
-import { createUser, getAllUsers, getUserById } from '../controller/userController.js';
+import { createUser, deleteUserData, getAllUsers, getUserById, updateUserData } from '../controller/userController.js';
 // import { get } from 'mongoose';
 
 const route = express.Router();
 route.post('/create-user', createUser)
 route.get('/get-users', getAllUsers)
 route.get('/get-users/:id', getUserById)
+route.put('/user-update/:id', updateUserData)
+route.delete('/user-delete/:id', deleteUserData )
 
 
 export default route;
